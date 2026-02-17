ab9e0a832627a885559e098bb76be2cacfc6e9c0
Ankit Dhakad <mynameisankitdhakad@gmail.com>
2026-02-17 22:32:18 +0530

    updated port config

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5001, () => {
-  console.log("Server running on http://localhost:5001");
+// server.listen(5001, () => {
+//   console.log("Server running on http://localhost:5001");
+// });
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
