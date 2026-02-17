3757f57b6162307a0f3dd46fd170e1002b0d6948
Ankit Dhakad <mynameisankitdhakad@gmail.com>
2026-02-17 22:25:09 +0530

    scoreboard project

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+let scoreboard = {
+  teamA: "TEAM A",
+  teamB: "TEAM B",
+  scoreA: 0,
+  scoreB: 0
+};
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  // Send current scoreboard to new user
+  socket.emit("updateScore", scoreboard);
+
+  // Admin updates score
+  socket.on("changeScore", (data) => {
+    scoreboard = data;
+    io.emit("updateScore", scoreboard);
+  });
+});
+
+server.listen(5001, () => {
+  console.log("Server running on http://localhost:5001");
+});
